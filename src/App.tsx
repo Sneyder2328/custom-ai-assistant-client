e670fa2a705129fa390b422c9a38991a5e3166f2
Sneyder Angulo <sneyder2328@gmail.com>
2023-08-10 11:38:36 -0400

    feat(Root): Set up react router and add the main routes definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const ProjectDashboard = lazy(
+  () => import("./pages/ProjectDashboard/index.jsx")
+);
+const SignUp = lazy(() => import("./pages/SignUp/index.js"));
+const Login = lazy(() => import("./pages/Login/index.jsx"));
+
 function App() {
-  return <div className="mx-10">hello world</div>;
+  return (
+    <main>
+      <Suspense fallback={<div />}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="projects/:projectId/*"
+              element={<ProjectDashboard />}
+            />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </main>
+  );
 }
 
 export default App;
